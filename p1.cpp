eaf3c7ce29e3a50012e91a212e9ce6927436303c
Minahil Imran <144251674+MiniCyberBrush@users.noreply.github.com>
2023-10-06 10:48:45 +0500

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+main(){
+char code; int price;
+cout << "Enter the vehicle type code (M, E, S, V, T): ";
+cin >> code;
+cout << "Enter the price of vehicle: ";
+cin >> price;
+
+
+}
+
+void pyramidVolume(float length, float width , float height , string unit)
+{
+if (unit == "milimeter")
+
+}
+if (unit == "centimeter")
+{
+
+}
+if (unit == "meter")
+
+}
+
+if (unit == "kilometer")
+{
+
+}
+}
